70cd4afabe7470141495999b993d048b750aea9d
Patrick Quinn-Graham <make-contact@pftqg.com>
2014-11-28 16:01:51 +1100

    Add build step to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,32 @@
+'use strict';
+
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var serve = require('gulp-serve');
+var uglify = require('gulp-uglify');
+var umd   = require('gulp-umd');
+var merge = require('merge-stream');
 
 gulp.task('serve', serve({ root: __dirname, port: 8100 }));
+
+gulp.task('default', function() {
+
+  var js = gulp.src('js/*.js')
+    .pipe(umd({
+      exports: function() {
+        return 'createOpentokHardwareSetupComponent';
+      },
+      namespace: function() {
+        return 'createOpentokHardwareSetupComponent'
+      }
+    }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('dist/'));
+
+  var css = gulp.src('css/*.css')
+    .pipe(gulp.dest('dist/'));
+
+  return merge(js, css);
+});
