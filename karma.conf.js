e5588c04ee381da1c5c1416a8d67ce80d11c7b6b
Michael Jolley <mike@sparcapp.io>
2020-12-19 10:19:18 -0600

    Updating tests for CI

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome', 'Firefox', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
 };
